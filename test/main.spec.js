33cae9294ff3a68ce6f0a4ef7ac95b35905a505b
japier <japier.07@gmail.com>
2023-09-14 07:05:41 -0600

    Adding testing to github actions

---
+++ test/main.spec.js
@@ -0,0 +1,9 @@
+test('string with a single number should result in the number itself', () => {
+  expect(1).toBe(1);
+});
+
+test('string with a single number should result in the number itself', () => {
+  expect(2).toBe(1);
+});
+
+
